3529011ee5383d391994d0141b4db213b9508784
yyeonkim <seyeon.dev@gmail.com>
2021-11-19 11:03:32 +0900

    Feat: Add form control and handle event

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,26 +1,60 @@
-import { Flex, Input, IconButton } from "@chakra-ui/react";
+import axios from "axios";
+
+import { useState } from "react";
+
+import { Input, IconButton, FormControl } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
 
 import styled from "styled-components";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
-console.log(API_KEY);
 
 export default function Search() {
+  const [keyword, setKeyword] = useState("");
+
+  const handleChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setKeyword(value);
+  };
+
+  const pressEnter = (event) => {
+    const { key } = event;
+    if (key === "Enter") {
+      setKeyword("");
+    }
+  };
+
+  const clickButton = () => {
+    setKeyword("");
+  };
+
   return (
-    <Flex w="80%" maxW="800" mt="10%" pos="relative">
+    <FormControl
+      id="keyword"
+      display="flex"
+      width="80%"
+      maxWidth="800"
+      marginTop="10%"
+      alignItems="center"
+      method="get"
+    >
       <Input
         p={5}
+        value={keyword}
         placeholder="검색어를 입력하세요"
         borderRadius="10"
         boxShadow="md"
+        onChange={(event) => handleChange(event)}
+        onKeyPress={(event) => pressEnter(event)}
       />
       <IconButton
-        pos="absolute"
         right="0"
         aria-label="Search database"
         icon={<SearchIcon />}
+        onClick={() => clickButton()}
       />
-    </Flex>
+    </FormControl>
   );
 }
