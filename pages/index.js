7814db59535aa2bc374298f1b357da82092b69dd
march0320 <march18529@gmail.com>
2021-11-27 04:15:53 +0900

    chore: install dotenv

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Description from "../src/components/Description";
 import Recommend from "../src/components/Recommend";
+import RelatedBook from "../src/components/RelatedBook";
 import { Box, Button } from "@chakra-ui/react";
 export default function Home() {
   return (
     <div>
       <Recommend />
+      <RelatedBook />
       <Description />
     </div>
   );
