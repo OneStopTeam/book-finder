8a764d45de6d1cd554fec8b2da40a4bf191208b4
march0320 <march18529@gmail.com>
2021-11-26 23:33:03 +0900

    style: Upgrade bookcover

--- src/components/Recommend.js
+++ src/components/Recommend.js
@@ -1,32 +1,46 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Button, Box, Text, Stack, Image } from "@chakra-ui/react";
-const API_KEY = "AIzaSyCgCD-h-9f4xPwqKCAj0pn6f5MPehFWV3I";
+import bookCover from "../../public/img/book-cover.png";
 
 export default function Recommend() {
   const [bookObj, setBookObj] = useState([]);
   const [loading, setLoading] = useState(false);
-  const author = "andy";
+  const author = "황순원";
 
   useEffect(() => {
     const book = async () => {
       setLoading(true);
       if (author) {
         const response = await axios.get(
-          `https://www.googleapis.com/books/v1/volumes?q=${author}`
+          `https://www.googleapis.com/books/v1/volumes?q=inauthor=${author}`
         );
         setBookObj(response.data.items);
       }
       setLoading(false);
     };
     book();
+    console.log(bookObj);
   }, []);
 
   if (loading) return <div>로딩중</div>;
   return (
     <Box width="80%">
       {bookObj.map((value, index) => (
-        <Text key={index}>{value.volumeInfo.title}</Text>
+        <Box position="relative" visibility="">
+          {value.volumeInfo.imageLinks ? (
+            <Image
+              w="10rem"
+              key={index}
+              alt={value.volumeInfo.title}
+              src={value.volumeInfo.imageLinks.thumbnail}
+              borderRadius="15"
+            />
+          ) : (
+            <Image src={bookCover.src} w="10rem" borderRadius="15" />
+          )}
+          <Text positon="absolute">{value.volumeInfo.title}</Text>
+        </Box>
       ))}
     </Box>
   );
