6e228c48f97793ea3424d73f49d15ccbc7e9f4fc
yyeonkim <seyeon.dev@gmail.com>
2021-12-04 02:43:09 +0900

    Fix: Pass bookData directly to return
    Do not read value of undefined

--- pages/detail/[id].js
+++ pages/detail/[id].js
@@ -1,6 +1,6 @@
 import { Flex, Text } from "@chakra-ui/react";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useGetBook from "../../src/hooks/useGetBook";
 
 export default function Detail() {
@@ -8,24 +8,23 @@ export default function Detail() {
   const {
     query: { id },
   } = router;
-  const { bookData, isLoading, isError, getBookData } = useGetBook(id);
-  useEffect(getBookData, [id]);
+  const { bookData, isLoading, getBookData } = useGetBook();
 
-  const {
-    volumeInfo: { title },
-  } = bookData;
+  useEffect(() => {
+    getBookData(id);
+  }, [id]);
 
-  if (isError) {
-    return <Text>XXX Error XXX</Text>;
-  } else {
-    return isLoading ? (
-      <Text>Loading...</Text>
-    ) : (
-      <Flex direction="column" align="center" pos="relative" w="100%">
-        <Text fontSize="xl" fontWeight="bold" color="orange.600" mb={2}>
-          Book: {title}
-        </Text>
-      </Flex>
-    );
-  }
+  // const {
+  //   volumeInfo: { title },
+  // } = bookData;
+
+  return isLoading ? (
+    <Text>Loading...</Text>
+  ) : (
+    <Flex direction="column" align="center" pos="relative" w="100%">
+      <Text fontSize="xl" fontWeight="bold" color="orange.600" mb={2}>
+        Book: {bookData && bookData.volumeInfo.title}
+      </Text>
+    </Flex>
+  );
 }
