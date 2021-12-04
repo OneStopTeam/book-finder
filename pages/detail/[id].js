8307163e3e53ec00082db94c3d26af96e70d55f2
yyeonkim <seyeon.dev@gmail.com>
2021-12-04 13:56:57 +0900

    Remove unused hook

--- pages/detail/[id].js
+++ pages/detail/[id].js
@@ -1,86 +1,14 @@
-<<<<<<< HEAD
-import { Flex,Button, Box, Text, Spinner, Image } from "@chakra-ui/react";
+import { Flex, Button, Box, Text, Image } from "@chakra-ui/react";
 import { useRouter } from "next/router";
-import Link from "next/link"
-import { useEffect, useState } from "react";
-import Search from "../../src/components/Search";
-
-
-export default function Detail() {
-<Search
-
-      />
-    const router = useRouter();
-  const {
-    query: { title, buylink,preview,img,date,publisher,authors,description},
-  } = router;
-
-    const BuyButton = () => {
-      return (
-        <Link href={buylink}>
-          <Button m={1}>구매하기</Button>
-        </Link>
-      );
-    };
-    console.log(authors);  
-    const PreviewButton = () => {
-      return (
-        <Link href={preview}>
-          <Button m={1}>미리보기</Button>
-        </Link>
-      );
-    };
-console.log(img);
-    return (
-        <Box>
-          <Flex borderRdius="15" justify="center" direction="column">
-            <Flex direction="row" p={20} m="1rem" justify="center">
-              <Box>
-                <Image
-                  boxShadow="md"
-                  borderRadius="1rem"
-                  src={img}
-                  p={1}
-                />
-                <Flex m={2} justify="center">
-                  <BuyButton />
-                  <PreviewButton />
-                </Flex>
-              </Box>
-              <Box m={3}>
-                <Text fontSize="2rem" fontWeight="bolder">
-                  {title}
-                </Text>
-                <Text m={1} color="#868e96">
-                  {authors}
-                  {publisher} |
-                  {date}
-                </Text>
-                <Box m={2}>
-                  <Text m={1}>책소개</Text>
-                  <Box h="17rem" maxW="40rem" overflow="auto">
-                    {description}
-                  
-                  </Box>
-                </Box>
-              </Box>
-            </Flex>
-          </Flex>
-        </Box>
-      );
-  
-}
-=======
-import { Flex, Text } from "@chakra-ui/react";
-import { useRouter } from "next/router";
+import Link from "next/link";
 import { useEffect } from "react";
+
 import useGetBook from "../../src/hooks/useGetBook";
 
 export default function Detail() {
-  const router = useRouter();
   const {
     query: { id },
-  } = router;
+  } = useRouter();
   const { bookData, isLoading, getBookData } = useGetBook();
 
   useEffect(() => {
@@ -90,15 +18,63 @@ export default function Detail() {
   // const {
   //   volumeInfo: { title },
   // } = bookData;
+  const {
+    query: {
+      title,
+      buylink,
+      preview,
+      img,
+      date,
+      publisher,
+      authors,
+      description,
+    },
+  } = router;
 
-  return isLoading ? (
-    <Text>Loading...</Text>
-  ) : (
-    <Flex direction="column" align="center" pos="relative" w="100%">
-      <Text fontSize="xl" fontWeight="bold" color="orange.600" mb={2}>
-        Book: {bookData && bookData.volumeInfo.title}
-      </Text>
-    </Flex>
+  const BuyButton = () => {
+    return (
+      <Link href={buylink}>
+        <Button m={1}>구매하기</Button>
+      </Link>
+    );
+  };
+
+  const PreviewButton = () => {
+    return (
+      <Link href={preview}>
+        <Button m={1}>미리보기</Button>
+      </Link>
+    );
+  };
+
+  return (
+    <Box>
+      <Flex borderRdius="15" justify="center" direction="column">
+        <Flex direction="row" p={20} m="1rem" justify="center">
+          <Box>
+            <Image boxShadow="md" borderRadius="1rem" src={img} p={1} />
+            <Flex m={2} justify="center">
+              <BuyButton />
+              <PreviewButton />
+            </Flex>
+          </Box>
+          <Box m={3}>
+            <Text fontSize="2rem" fontWeight="bolder">
+              {title}
+            </Text>
+            <Text m={1} color="#868e96">
+              {authors}
+              {publisher} |{date}
+            </Text>
+            <Box m={2}>
+              <Text m={1}>책소개</Text>
+              <Box h="17rem" maxW="40rem" overflow="auto">
+                {description}
+              </Box>
+            </Box>
+          </Box>
+        </Flex>
+      </Flex>
+    </Box>
   );
 }
->>>>>>> 22fc909de6a5195d1c5a3d9b8998b4bf04f17695
--- src/components/Recommend.js
+++ src/components/Recommend.js
@@ -1,12 +1,13 @@
 import axios from "axios";
-import Link from "next/link"
-import {useRecoilState}from "recoil";
+import Link from "next/link";
+import { useRecoilState } from "recoil";
 import { useEffect, useState } from "react";
 import { Flex, Grid, Box, Text, Spinner, Image } from "@chakra-ui/react";
-import bookCover from "../../public/img/book-cover.png";
 import { relatedState } from "./stage";
 
-export default function Recommend() { 
+import bookCover from "../../public/img/book-cover.png";
+
+export default function Recommend() {
   const [related, setRelated] = useRecoilState(relatedState);
   const [loading, setLoading] = useState(false);
   const [isError, setIsError] = useState(null);
@@ -32,30 +33,41 @@ export default function Recommend() {
       }
     };
     book();
-   
   }, [setRelated]);
+
   console.log(related);
-console.log(relatedState.default);
+  console.log(relatedState.default);
+
   if (loading)
     return (
       <Flex justify="center">
         <Spinner mt="10%" color="red.500" thickness="4px" size="xl" />
       </Flex>
     );
+
   return (
     <Grid templateColumns="repeat(4,1fr)" m={10}>
-<<<<<<< HEAD
       {related &&
         related.map((book, index) => (
-          <Link book={book} href={{pathname:`/detail/[id]`, query:{id:book.id,
-          title:book.volumeInfo.title,
-          buylink:book.saleInfo.buyLink,
-          description:book.volumeInfo.description,
-          preview:book.volumeInfo.previewLink,
-          img:((book.volumeInfo.imageLinks)? book.volumeInfo.imageLinks.small: bookCover.src),
-          date : book.volumeInfo.publishedDate,
-          publisher:book.volumeInfo.publisher,
-          authors:book.volumeInfo.authors}}}>
+          <Link
+            book={book}
+            href={{
+              pathname: `/detail/[id]`,
+              query: {
+                id: book.id,
+                title: book.volumeInfo.title,
+                buylink: book.saleInfo.buyLink,
+                description: book.volumeInfo.description,
+                preview: book.volumeInfo.previewLink,
+                img: book.volumeInfo.imageLinks
+                  ? book.volumeInfo.imageLinks.small
+                  : bookCover.src,
+                date: book.volumeInfo.publishedDate,
+                publisher: book.volumeInfo.publisher,
+                authors: book.volumeInfo.authors,
+              },
+            }}
+          >
             <Box key={index} book={book} position="relative" visibility="">
               {book.volumeInfo.imageLinks ? (
                 <Image
@@ -87,40 +99,6 @@ console.log(relatedState.default);
             </Box>
           </Link>
         ))}
-=======
-      {related.map((value, index) => (
-        <Box key={index} position="relative" visibility="">
-          {value.volumeInfo.imageLinks ? (
-            <Image
-              h="15rem"
-              key={index}
-              alt={value.volumeInfo.title}
-              src={value.volumeInfo.imageLinks.thumbnail}
-              borderRadius="15"
-              boxShadow="md"
-            />
-          ) : (
-            <Image
-              src={bookCover.src}
-              h="15rem"
-              boxShadow="md"
-              borderRadius="15"
-            />
-          )}
-          <Text
-            p={1}
-            display="flex"
-            justify="center"
-            fontSize="0.7rem"
-            w="10rem"
-            color="#868e96"
-            position="absolute"
-          >
-            {value.volumeInfo.title}
-          </Text>
-        </Box>
-      ))}
->>>>>>> 22fc909de6a5195d1c5a3d9b8998b4bf04f17695
     </Grid>
   );
 }
