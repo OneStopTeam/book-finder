ac7ae09eb1ad5853473d2b34ace4f582f5f8a3af
Yongyeon Kim <yeonlotus@ewhain.net>
2022-03-25 12:41:36 +0900

    Fix: Disable button when buy link is undefined

--- src/components/Books/SearchedBook.js
+++ src/components/Books/SearchedBook.js
@@ -1,5 +1,6 @@
 import { Image, Text } from "@chakra-ui/react";
 import BuyButton from "../Buttons/BuyButton";
+import { IoLogoGooglePlaystore } from "react-icons/io5";
 
 export default function SearchedBook({ book }) {
   console.log(book);
@@ -16,15 +17,17 @@ export default function SearchedBook({ book }) {
       />
       <Text>{title}</Text>
       <Text>{authors ? authors.join(", ") : "작자 미상"} / 평점</Text>
-      <Text display="flex">
+      <Text display="flex" alignItems="center">
         판매가 &nbsp;
         {retailPrice ? (
           <Text color="accent">{retailPrice.amount}원</Text>
         ) : (
-          "알수없음"
+          "정보없음"
         )}
+        &nbsp;
+        <IoLogoGooglePlaystore />
       </Text>
-      <BuyButton link={buyLink} />
+      <BuyButton link={buyLink} isDisabled={retailPrice ? false : true} />
     </>
   );
 }
--- src/components/Buttons/BuyButton.js
+++ src/components/Buttons/BuyButton.js
@@ -1,12 +1,18 @@
 import { Button } from "@chakra-ui/react";
 
-export default function BuyButton({ link }) {
+export default function BuyButton({ link, isDisabled }) {
   const onClick = () => {
     window.open(link);
   };
 
   return (
-    <Button bg="accent" color="white" size="sm" onClick={onClick}>
+    <Button
+      bg="accent"
+      color="white"
+      size="sm"
+      onClick={onClick}
+      isDisabled={isDisabled}
+    >
       구매하기
     </Button>
   );
