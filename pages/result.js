8d59748ff74b0e18cf5d0947060f30e530d7cd5b
Yongyeon Kim <yeonlotus@ewhain.net>
2022-04-01 09:06:54 +0900

    Add hook for getting the number of pages

--- pages/result.js
+++ pages/result.js
@@ -1,9 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Box, Center, Flex, Text } from "@chakra-ui/react";
 import { useQuery } from "react-query";
 import { useRecoilValue } from "recoil";
 import { useRouter } from "next/router";
-import axios from "axios";
 
 import Search from "../src/components/Search";
 import { keywordState, pageState } from "../src/atom";
@@ -11,37 +10,25 @@ import SearchedBook from "../src/components/Books/SearchedBook";
 import { fetchResult } from "../src/fetching";
 import useWindowDimensions from "../src/hooks/useWindowDimensions";
 import Pagination from "../src/components/Pagination";
+import useGetNumberOfData from "../src/hooks/useGetNumberOfData";
 
 export default function Result() {
   const router = useRouter();
   const keyword = useRecoilValue(keywordState); // 검색어
   const startIndex = useRecoilValue(pageState); // start index for fetching
-  const [numberOfData, setNumberOfData] = useState(0);
-
   // keyword로 구글 도서 정보 가져오기
-  const { data, isLoading, isError } = useQuery([startIndex, keyword], () =>
-    fetchResult(startIndex, keyword)
-  );
+  const {
+    data,
+    isLoading: resultIsLoading,
+    isError,
+  } = useQuery([startIndex, keyword], () => fetchResult(startIndex, keyword));
   const { height: windowHeight } = useWindowDimensions();
 
   // 검색 결과 개수 구하기
-  useEffect(async () => {
-    let count = 0; // 검색 결과 개수
+  const { numberOfData, isLoading: numberIsLoading } =
+    useGetNumberOfData(keyword);
 
-    for (let i = 0; count < 400; i++ * 40) {
-      const {
-        data: { items },
-      } = await axios.get(
-        `https://www.googleapis.com/books/v1/volumes?q=${keyword}&startIndex=${i}&maxResults=40&key=${process.env.NEXT_PUBLIC_API_KEY}`
-      );
-      // items이 더 이상 없으면 종료
-      if (!items) break;
-
-      count += items.length;
-    }
-
-    setNumberOfData(count);
-  }, [keyword]);
+  const isLoading = resultIsLoading || numberIsLoading;
 
   useEffect(() => {
     // 검색어가 없으면 홈으로 이동
---
+++ src/hooks/useGetNumberOfData.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function useGetNumberOfData(keyword) {
+  const [numberOfData, setNumberOfData] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+
+  // 검색 결과 개수 구하기
+  useEffect(async () => {
+    let count = 0; // 검색 결과 개수
+
+    for (let i = 0; count < 200; i++ * 40) {
+      const {
+        data: { items },
+      } = await axios.get(
+        `https://www.googleapis.com/books/v1/volumes?q=${keyword}&startIndex=${i}&maxResults=40&key=${process.env.NEXT_PUBLIC_API_KEY}`
+      );
+      // items이 더 이상 없으면 종료
+      if (!items) break;
+
+      count += items.length;
+    }
+
+    setNumberOfData(count);
+    setIsLoading(false);
+  }, [keyword]);
+
+  return { numberOfData, isLoading };
+}
