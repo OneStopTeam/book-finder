60a5db095fe221793f565de5b5f43c92dd83d2c0
Yongyeon Kim <yeonlotus@ewhain.net>
2022-04-01 13:27:59 +0900

    Feat: Add next, before button to move page

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,4 +1,5 @@
 import { Circle, HStack } from "@chakra-ui/react";
+import { ChevronRightIcon, ChevronLeftIcon } from "@chakra-ui/icons";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import { useSetRecoilState } from "recoil";
@@ -10,20 +11,49 @@ export default function Pagination({ numberOfPages }) {
   const setStartIndex = useSetRecoilState(pageState);
   const [pageArr, setPageArr] = useState(Array(numberOfPages).fill(0)); // pagination 반복을 줄이기 위한 array
   const [currentPage, setCurrentPage] = useState("1");
+  const [isNextPage, setIsNextPage] = useState(false);
 
   // page 번호 설정
-  const clickPage = (event) => {
-    const page = event.target.textContent;
+  const movePage = (page) => {
     setStartIndex((page - 1) * 20);
     setCurrentPage(page);
     router.push(`/result?page=${page}`);
   };
 
+  // pagination 생성
+  const createPages = () => {
+    if (pageArr.length < 5) {
+      return pageArr;
+    }
+    if (isNextPage) {
+      return pageArr.slice(5);
+    } else {
+      return pageArr.slice(0, 5);
+    }
+  };
+
+  // 이전 or 다음 버튼 클릭 시 page 버튼 변경
+  const changePages = (where) => {
+    setIsNextPage(where === "next" ? true : false);
+    const page = where === "next" ? "6" : "5";
+    movePage(page);
+  };
+
   return (
     <HStack spacing={3} mb="2rem">
-      {pageArr.map((element, index) => {
-        // 현재 페이지
-        if (`${index + 1}` === currentPage) {
+      {/* 이전 페이지 버튼 */}
+      {isNextPage && (
+        <ChevronLeftIcon
+          boxSize="3rem"
+          color="accent"
+          _hover={{ cursor: "pointer" }}
+          onClick={() => changePages("before")}
+        />
+      )}
+      {createPages().map((element, index) => {
+        const pageNumber = isNextPage ? index + 6 : index + 1;
+        // 현재 페이지 스타일 지정
+        if (`${pageNumber}` === currentPage) {
           return (
             <Circle
               _hover={{ cursor: "pointer" }}
@@ -32,9 +62,9 @@ export default function Pagination({ numberOfPages }) {
               color="white"
               borderColor="accent"
               bg="accent"
-              onClick={(event) => clickPage(event)}
+              onClick={(event) => movePage(event.target.textContent)}
             >
-              {index + 1}
+              {pageNumber}
             </Circle>
           );
         }
@@ -46,12 +76,22 @@ export default function Pagination({ numberOfPages }) {
             border="2px"
             color="black"
             borderColor="accent"
-            onClick={(event) => clickPage(event)}
+            onClick={(event) => movePage(event.target.textContent)}
           >
-            {index + 1}
+            {pageNumber}
           </Circle>
         );
       })}
+
+      {/* 다음 페이지 버튼 */}
+      {!isNextPage && pageArr.length > 5 && (
+        <ChevronRightIcon
+          boxSize="3rem"
+          color="accent"
+          _hover={{ cursor: "pointer" }}
+          onClick={() => changePages("next")}
+        />
+      )}
     </HStack>
   );
 }
