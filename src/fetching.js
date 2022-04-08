894c0430d224a380ae96e7ed518117381af91dbe
Yongyeon Kim <yeonlotus@ewhain.net>
2022-04-08 11:03:22 +0900

    Rename google api key
    API KEY --> GOOGLE API KEY

--- src/fetching.js
+++ src/fetching.js
@@ -5,7 +5,7 @@ export const fetchResult = async (startIndex, keyword) => {
     data: { items },
   } = await axios
     .get(
-      `https://www.googleapis.com/books/v1/volumes?q=${keyword}&startIndex=${startIndex}&maxResults=20&key=${process.env.NEXT_PUBLIC_API_KEY}`
+      `https://www.googleapis.com/books/v1/volumes?q=${keyword}&startIndex=${startIndex}&maxResults=20&key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}`
     )
     .catch((error) => {
       console.log(error.response);
